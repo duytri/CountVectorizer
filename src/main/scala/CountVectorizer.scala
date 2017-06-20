825da6e10057132a22550173449163507fd890ca
nguyenduy <nguyenduy@NGUYENDUY-UBUNTU>
2017-06-20 20:43:00 +0700

    Initial project

---
+++ src/main/scala/CountVectorizer.scala
@@ -0,0 +1,15 @@
+package main.scala
+
+class CountVectorizer {
+  def main(args: Array[String]): Unit = {
+
+  }
+
+  def printHelp() = {
+    println("Usage: CountVectorizer [Arguments]")
+    println("       Arguments:")
+    println("              -i : Input file path or folder path in HDFS")
+    println("              -o : Outout file path in HDFS")
+    println("              -h : Print this help")
+  }
+}
